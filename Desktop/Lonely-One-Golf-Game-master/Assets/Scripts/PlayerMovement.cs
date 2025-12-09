f525d545ded6798ece7aba4c4b592653f651e427
Souvik <souvik.sural@capsitech.com>
2025-12-09 12:28:02 +0530

    Hole in one Feature implemented

--- Desktop/Lonely-One-Golf-Game-master/Assets/Scripts/PlayerMovement.cs
+++ Desktop/Lonely-One-Golf-Game-master/Assets/Scripts/PlayerMovement.cs
@@ -15,15 +15,18 @@ public class PlayerMovement : MonoBehaviour
     public ParticleSystem impactEffect;
     public RectTransform cancelButtonRect;
     public FingerTrajectoryTutorial tutorial;
-
-
+    public int shotsUsed = 0;
     private bool firstHitDone = false;
     private bool dragApplied = false;
     public TrailRenderer trail;
+    private bool inputBlocked = false;
 
     public GameObject restartPanel;
     public bool touchedGround = false;
-    
+    public bool holeInOnePossible = true;
+
+    public bool ballStoppedAfterFirstShot = false;
+
     public Button cancelButton;
     public float cancelTriggerPercent = 0.15f;
 
@@ -35,6 +38,8 @@ public class PlayerMovement : MonoBehaviour
         trail.emitting = true;
         firstHitDone = false;
         trail.Clear();
+        shotsUsed = 0;
+        holeInOnePossible = true;
         restartPanel.SetActive(false);
 
         if (cancelButton != null)
@@ -81,7 +86,8 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (!dragging && Input.GetMouseButtonDown(0))
+        if (!dragging && !inputBlocked && Input.GetMouseButtonDown(0))
+
         {
             FingerTrajectoryTutorial tut = FindFirstObjectByType<FingerTrajectoryTutorial>();
             if (tut != null)
@@ -108,14 +114,35 @@ public class PlayerMovement : MonoBehaviour
             FinishShot();
         }
 
-        if (rb.linearVelocity.magnitude < 0.1f && firstHitDone)
+        if (!ballStoppedAfterFirstShot &&
+     firstHitDone &&
+     rb.linearVelocity.magnitude < 0.1f &&
+     shotsUsed == 1)
         {
-            trail.emitting = false;
+            ballStoppedAfterFirstShot = true;
+            StartCoroutine(HandleFirstShotFail());
         }
+
     }
+    private IEnumerator HandleFirstShotFail()
+    {
+        yield return new WaitForSeconds(2f); 
+       
+        if (!holeInOnePossible) yield break;
+
+        LiveManager lm = FindAnyObjectByType<LiveManager>();
+        if (lm != null)
+            lm.LoseLife();
+    }
+
+
+ 
 
     void FinishShot()
     {
+        ballStoppedAfterFirstShot = false;
+        inputBlocked = true;
+        trajectory.Hide();
         lr.positionCount = 0;
         dragging = false;
 
@@ -135,6 +162,10 @@ public class PlayerMovement : MonoBehaviour
                 trail.emitting = true;
                 firstHitDone = true;
             }
+            shotsUsed++;
+            if (shotsUsed > 1)
+                holeInOnePossible = false;
+
         }
 
         trajectory.Hide();
@@ -215,9 +246,14 @@ public class PlayerMovement : MonoBehaviour
         rb.angularVelocity = 0f;
 
         trail.Clear();
+        inputBlocked = false;
+        trajectory.Hide();
 
         firstHitDone = false;
-        trail.emitting = false;  
+        trail.emitting = false;
+        shotsUsed = 0;
+        holeInOnePossible =  true;
+        ballStoppedAfterFirstShot = false;
     }
      void FixedUpdate()
     {
